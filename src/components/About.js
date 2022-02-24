253418edbd1db699209372172d332c764aac3109
cedric karungu <ckarungu921@gmail.com>
2022-02-24 11:17:37 +0200

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h4>Version 1.0.0</h4>
+    <p>
+        This app is buid by <strong>Cedric KARUNGU</strong> <br/>
+        <a href='/'>Home</a>
+    </p>
+    </div>
+  )
+}
+
+export default About
